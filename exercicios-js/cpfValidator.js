718b4275f0daaad9887ec77a8825222300b556cc
Jonas Cesconetto <jonascesconetto@edu.univali.br>
2020-09-15 16:45:49 -0300

    Nova organização do diretorio

---
+++ exercicios-js/cpfValidator.js
@@ -0,0 +1,46 @@
+function TestaCPF(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+
+    if (strCPF == "00000000000"){ 
+        return false;
+    }
+
+    for (i = 1; i <= 9; i++) {
+        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+        Resto = (Soma * 10) % 11;
+    }
+
+    if ((Resto == 10) || (Resto == 11)){
+        Resto = 0;
+    }
+
+    if (Resto != parseInt(strCPF.substring(9, 10))){
+      return false;  
+    } 
+
+    Soma = 0;
+
+    for (i = 1; i <= 10; i++) {
+        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
+    }
+
+    if ((Resto == 10) || (Resto == 11)){
+        Resto = 0;
+    }
+
+    if (Resto != parseInt(strCPF.substring(10, 11))) {
+        return false;
+    }
+
+    return true;
+}
+
+var strCPF = "123876904";
+console.log(TestaCPF(strCPF));
+
+var str = '009.355.869-46';
+console.log(str)
+console.log( str.replace(/[^\d]+/g,'') )
