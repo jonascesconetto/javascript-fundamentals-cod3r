7a0957c44e78c148363cd40d83050acaf0979efe
Jonas Cesconetto <jonascesconetto@gmail.com>
2023-01-31 14:03:33 -0300

    Feat: introdução a orientação a objetos

---
+++ 07_objeto/oo.js
@@ -0,0 +1,30 @@
+// Código não executável
+
+// Programação não estruturada
+//  - Go-To
+
+// Procedural
+//  - Procedimento ou função
+funcao(param1, param2)
+
+// Orientação a Objetos
+// Dados que possuem funções
+// Objeto: capsula que agrupa comportamentos e atributos
+objeto = {
+    param1, 
+    param2,
+    valor1,
+    funcao(){
+        ...,
+        ...
+    }
+}
+
+objeto.funcao()
+
+// Principios Importantes de OO
+// 1. Abstração
+// 2. Encapsulamento
+//  - Auto acoplamento não é bom (principio clean code)
+// 3. Herança (prototype)
+// 4. Polimorfismo
